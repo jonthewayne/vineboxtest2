2aa32dabcc1d79406d5c98d337818492432515f3
Jon Shumate <jonthewayne@gmail.com>
2013-04-05 11:40:28 -0700

    rails_apps_composer: initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Vineboxtest2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vineboxtest2::Application.config.secret_token = '57836555056b12e656f573cfeba49e38040b25d3d18cd6eff00a60a305abaf67c3738cdc8f2e77fda7819ec046b3b5f4edb9340ea7f48d0a2ce9fa70ca0df82c'
